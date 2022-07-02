6d841f8b56ce35b9259c2e78d695c0ba5edeb289
iammardonbek <100706341+iammardonbek@users.noreply.github.com>
2022-07-02 10:34:33 +0500

    hello

--- src/components/Home/Category/Card/Card.jsx
+++ src/components/Home/Category/Card/Card.jsx
@@ -4,7 +4,7 @@ import House from "../../../../Assets/house.png";
 import { useNavigate } from "react-router-dom";
 const Card = ({ img, title, icon, id }) => {
   const navigate = useNavigate();
-  var goTo = () => navigate("/properties");
+  var goTo = () => navigate(`/properties?category_id=${id}`);
   return (
     <CardWrapper id={id} onClick={goTo}>
       <img src={img ? img : House} alt="house" />
